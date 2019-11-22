35a4df8bd6e4d01987e64151fecc0edecea975fa
WQY <1272722954@qq.com>
2019-11-22 09:51:35 +0800

    ont

---
+++ nettytest/src/test/java/com/github/nettytest/NettytestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nettytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettytestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
